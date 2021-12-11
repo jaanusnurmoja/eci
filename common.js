48ce8d012d7b07b901d930716a8c4b7a37ea728b
Jaanus Nurmoja <jaanus.nurmoja@gmail.com>
2021-12-11 13:28:01 +0200

    trying this

--- common.js
+++ common.js
@@ -1,5 +1,5 @@
-        var opts = "<option value=''>Show country</option>";
-        const staticCountryNames = {
+        var options = "<option value=''>Show country</option>";
+        const staticCountryList = {
             at: "Austria",
             be: "Belgium",
             bg: "Bulgaria",
@@ -30,31 +30,30 @@
             gb: "United Kingdom"
         };
 
-        for (var cc in staticCountryNames) {
-            opts += "<option value='" + cc + "'>" + staticCountryNames[cc] + "</option>";
+        for (var cc in staticCountryList) {
+            options += "<option value='" + cc + "'>" + staticCountryList[cc] + "</option>";
 
         }
-        document.getElementById("countryList").innerHTML = opts;
-        var selectedCountry = "";
+        document.getElementById("countryListExt").innerHTML = options;
+            var xmlhttpext = new XMLHttpRequest();
+            xmlhttpext.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    var myObj = JSON.parse(this.responseText);
+                    var i;
+                    var list = "<option value='en'>Choose language</option>";
+                    for (i = 0; i < myObj.languages.length; i++) {
+                        list += "<option value='" + myObj.languages[i].languageCode + "'> " + myObj.languages[i].languageName + "</option>"
 
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function () {
-            if (this.readyState == 4 && this.status == 200) {
-                var myObj = JSON.parse(this.responseText);
-                var i;
-                var list = "<option value='en'>Choose other language</option>";
-                for (i = 0; i < myObj.languages.length; i++) {
-                    list += "<option value='" + myObj.languages[i].languageCode + "'> " + myObj.languages[i].languageName + "</option>"
-
+                    }
+                    document.getElementById("langListExt").innerHTML = list;
                 }
-                document.getElementById("langlist").innerHTML = list;
-            }
-        };
-        xmlhttp.open("GET", "https://kodanikupalk.ee/varia/eci/languages.json", true);
-        xmlhttp.send();
+            };
+            xmlhttpext.open("GET", "/varia/eci/languages.json", true);
+            xmlhttpext.send();
+        
 
-        function loadJSON() {
-            var lang = document.getElementById("langlist").value;
+        function loadJSONExt(prop = 'levelGoalPercentage', asc) {
+            var lang = document.getElementById("langListExt").value;
             if (lang != "") {
                 lang = "?language=" + lang;
             }
@@ -82,6 +81,7 @@
             }
 
             http_request.onreadystatechange = function () {
+                var row = "";
 
                 if (http_request.readyState == 4) {
                     // Javascript function JSON.parse to parse JSON data
@@ -89,41 +89,144 @@
 
                     // jsonObj variable now contains the data structure and can
                     // be accessed as jsonObj.name and jsonObj.country.
-                    document.getElementById("time").innerHTML = "<h4>@ " + jsonObj.when + "</h4>";
-                    document.getElementById("signatureCountHeader").innerHTML = "<h1>Signatures: " + jsonObj.totalVotes.signatureCount + "</h1>";
-                    document.getElementById("goal").innerHTML = "<h1>required: " + jsonObj.totalVotes.goal + "</h1>";
-                    document.getElementById("statement").innerHTML = "<h3><a href='" + jsonObj.signUrl + "en' target='_blank'>Join with them! sign.eci-ubi.eu</a></h3>";
                     var countriesJson = jsonObj.countryVotes.signatureCountryCount;
-                    countriesJson.sort((a, b) => (a.totalCount / a.treshold < b.totalCount / b.treshold) ? 1 : (a.totalCount / a.treshold === b.totalCount / b.treshold) ? ((a.treshold > b.treshold) ? 1 : -1) : -1);
+                    var targets = jsonObj.targets;
+                    /* countriesJson.sort((a, b) => (a.count / a.treshold < b.count / b.treshold) ? 1 : (a.count / a.treshold === b.count / b.treshold) ? ((a.treshold > b.treshold) ? 1 : -1) : -1 ); */
                     var countryNames = jsonObj.countryNames;
-                    var row = "";
+                    var tableRow = [];
                     for (var i = 0; i < countriesJson.length; i++) {
                         var cc = countriesJson[i].countryCode;
-                        row += "<tr><td style='border: 0.5px solid black;text-align:center;'><img src='https://www.flagcdn.com/32x24/" + cc + ".png'></td><td style='text-align:right;border: 0.5px solid black;padding:4px'>" + (i+1) + "</td><td style='text-align:center;border: 0.5px solid black;padding:4px'>" + countryNames[cc] + "</td>"
-                            + "<td style='width:15%;text-align:right;border: 0.5px solid black;padding:4px'>" + countriesJson[i].totalCount + "</td>"
-                            + "<td style='width:10%;text-align:right;border: 0.5px solid black;padding:4px'>" + countriesJson[i].treshold + "</td>"
-                            + "<td style='text-align:right;border: 0.5px solid black;padding:4px'>" + parseFloat(countriesJson[i].percentage).toFixed(2) + "</td></tr>";
-
-                        if (document.getElementById("countryList").value == "") {
-                            selectedCountry = "ee";
-                        } else {
-                            selectedCountry = document.getElementById("countryList").value;
-                        }
-                        if (cc == selectedCountry) {
-                            var ee = "<h1>" + countryNames[cc] + ": <span style='white-space:nowrap'>" + countriesJson[i].totalCount + " / " + countriesJson[i].treshold + "</span></h1>";
-                            document.getElementById("selectedCountryCount").innerHTML = ee;
+                        tableRow[i] = {};
+                        tableRow[i].countryFlag = "<img src='https://www.flagcdn.com/32x24/" + cc + ".png'>";
+                        tableRow[i].countryName = countryNames[cc];
+                        tableRow[i].signatureCount = countriesJson[i].totalCount;
+                        tableRow[i].today = targets[cc].today;
+                        tableRow[i].level1Value = targets[cc].Level1.value;
+                        tableRow[i].level1Type = targets[cc].Level1.type;
+                        tableRow[i].level1Percentage = parseFloat(100 * countriesJson[i].totalCount / targets[cc].Level1.value).toFixed(2);
+                        tableRow[i].level2Value = targets[cc].Level2.value;
+                        tableRow[i].level2Type = targets[cc].Level2.type;
+                        tableRow[i].level2Percentage = parseFloat(100 * countriesJson[i].totalCount / targets[cc].Level2.value).toFixed(2);
+                        tableRow[i].levelGoalValue = targets[cc].LevelGoal.value;
+                        //tableRow[i].levelGoalType = targets[cc].LevelGoal.type;
+                        tableRow[i].levelGoalNeed = needPerDay(targets[cc].LevelGoal.value, countriesJson[i].totalCount);
+                        tableRow[i].levelGoalPercentage = parseFloat(100 * countriesJson[i].totalCount / targets[cc].LevelGoal.value).toFixed(2);
+                        tableRow[i].level3Value = targets[cc].Level3.value;
+                        tableRow[i].level3Type = targets[cc].Level3.type;
+                        tableRow[i].level3Percentage = parseFloat(100 * countriesJson[i].totalCount / targets[cc].Level3.value).toFixed(2);
+                        if (document.getElementById("countryListExt").value != "" && cc == document.getElementById("countryListExt").value) {
+                            countryData = "<h1>" + countryNames[cc] + ": " + tableRow[i].signatureCount + "</h1>";
                         }
                     }
-                    document.getElementById("countryVotes").getElementsByTagName("tbody")[0].innerHTML = row;
 
+                    function needPerDay(target, signatures) {
+                        var startDate = new Date("2020-09-25");
+                        var endDate = new Date("2021-12-25");
+                        var today = new Date();
+                        var firstDate = today;
+                        var days = [];
+                        while (today <= endDate) {
+                            // this line modifies the original firstDate reference which you want to make the while loop work
+                            firstDate.setDate(firstDate.getDate() + 1);
+                            // this pushes a new date , if you were to push firstDate then you will keep updating every item in the array
+                            days.push(new Date(firstDate));
+                        }
+                        var remained = days.length;
+                        var need = (target - signatures) / remained;
+                        return Math.round(need);
+                    }
+
+                    function sortSignaturData(prop, asc) {
+                        return function (a, b) {
+                            if (asc == true) {
+                                return a[prop] - b[prop];
+                            }
+                            else {
+                                return b[prop] - a[prop];
+                            }
+                        }
+                    }
+
+                    tableRow.sort(sortSignaturData(prop, asc));
+
+                    var rowCols = [];
+                    var align = "";
+                    for (var i = 0; i < tableRow.length; i++) {
+                        rowCols[i] = "";
+                        for ([key, value] of Object.entries(tableRow[i])) {
+                            align = key == "countryFlag" || key == "countryName" ? "center" : "right";
+                            if (key.includes("Percentage") && value >= 100) {
+                                value = "<span style='background-color:green;color:yellow;font-weight: bold;'>" + value + "</span>";
+                            }
+                            rowCols[i] += "<td style='text-align:" + align + ";border: 1px solid black;padding:0 6px'>" + value + "</td>";
+                        };
+
+
+                        var cc = countriesJson[i].countryCode;
+                        row += "<tr id='" + cc + "'>" + rowCols[i] + "</tr>";
+                    }
+                    console.log(rowCols);
+                    document.getElementById("countryVotesExtended").getElementsByTagName("tbody")[0].innerHTML = row;
+                    var allEU = "<tr>"
+                        + "<td style='border: 1px solid black;text-align:center;min-width:34px;'><img src='https://www.countryflags.io/eu/shiny/32.png'>"
+                        + "</td>"
+                        + "<td style='border: 1px solid black;text-align:center;padding:6px'>EU27"
+                        + "</td>"
+                        + "<td style='text-align:right;border: 1px solid black;padding:6px'>"
+                        + jsonObj.totalVotes.signatureCount
+                        + "</td>"
+                        + "<td style='text-align:right;border: 1px solid black;padding:0 6px'>"
+                        + targets.all.today
+                        + "</td>"
+                        + "<td style='text-align:center;border: 1px solid black;padding:6px' colspan='2'>"
+                        + targets.all.Level1.value
+                        + "</td>"
+                        + "<td style='text-align:right;border: 1px solid black;padding:6px'>"
+                        + parseFloat(100 * jsonObj.totalVotes.signatureCount / targets.all.Level1.value).toFixed(2)
+                        + "</td>"
+
+                        + "<td style='text-align:center;border: 1px solid black;padding:6px' colspan='2'>"
+                        + targets.all.Level2.value
+                        + "</td>"
+                        + "<td style='text-align:right;border: 1px solid black;padding:6px'>"
+                        + parseFloat(100 * jsonObj.totalVotes.signatureCount / targets.all.Level2.value).toFixed(2)
+                        + "</td>"
+
+                        + "<td style='text-align:center;border: 1px solid black;padding:6px'>"
+                        + targets.all.LevelGoal.value
+                        + "</td>"
+                        + "<td style='text-align:right;border: 1px solid black;padding:6px'>"
+                        + needPerDay(targets.all.LevelGoal.value, jsonObj.totalVotes.signatureCount)
+                        + "</td>"
+                        + "<td style='text-align:right;border: 1px solid black;padding:6px'>"
+                        + parseFloat(100 * jsonObj.totalVotes.signatureCount / targets.all.LevelGoal.value).toFixed(2)
+                        + "</td>"
+                        + "<td style='text-align:center;border: 1px solid black;padding:6px' colspan='2'>"
+                        + targets.all.Level3.value
+                        + "</td>"
+                        + "<td style='text-align:right;border: 1px solid black;padding:6px'>"
+                        + parseFloat(100 * jsonObj.totalVotes.signatureCount / targets.all.Level3.value).toFixed(2)
+                        + "</td>"
+
+
+                        + "</tr>"
+                    document.getElementById("countryVotesExtended").getElementsByTagName("tfoot")[0].innerHTML = allEU;
+                    document.getElementById("update").innerHTML = "<h2>EU: " + jsonObj.totalVotes.signatureCount + "</h2>" + countryData + "<h4>@ " + jsonObj.when + "</h4>";
                 }
             }
 
             http_request.open("GET", data_file, true);
             http_request.send();
 
-
         }
 
-        window.onload = loadJSON();
-        setInterval(loadJSON(), 3000);
+        function prop(name = 'levelGoalPercentage', asc) {
+            var a = asc == true ? "" : ", true";
+            var b = asc == true ? ", true" : "";
+            document.getElementById(name).setAttribute('onclick', "prop('" + name + "'" + a + "); return false;");
+            document.getElementById("update").setAttribute('onclick', "loadJSONExt('" + name + "'" + b + ");");
+            return loadJSONExt(name, asc);
+        }
+        window.onload = loadJSONExt();
+        setInterval(loadJSONExt(), 3000);
+
